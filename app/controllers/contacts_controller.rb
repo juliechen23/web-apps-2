aa92134b8093b0a131c221e717a8878c9221bf5a
juliechen23 <julie.chen@kellogg.northwestern.edu>
2023-02-14 22:32:55 +0000

    class 7

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,4 +9,15 @@ class ContactsController < ApplicationController
     @contact = Contact.new
   end
 
+  def create
+    @contact = Contact.new
+    @contact["first_name"] = parmas["contact"]["first_name"]
+    @contact["last_name"] = parmas["contact"]["last_name"]
+    @contact["email"] = parmas["contact"]["email"]
+    @contact["phone_number"] = parmas["contact"]["phone_number"]
+    @contact["company_id"] = parmas["contact"]["company_id"]
+    @contact.save
+    redirect_to "/companies/#{@contact["company_id"]}"
+end
+
 end
